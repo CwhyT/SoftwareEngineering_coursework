fc9542662abee1f8385f7d0b3f978bcc706fb687
helloworld <163817883+Hellooooooo-World@users.noreply.github.com>
2025-05-11 16:30:45 +0800

    Delete frontend directory

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-import '@/assets/bootstrap/css/bootstrap.min.css';
-import '@/assets/bootstrap/js/bootstrap.bundle.min.js';
-
-createApp(App).use(router).mount('#app');
