d6b84b2961d00ff2d6f792048e36feb9ff75a04c
helloworld <163817883+Hellooooooo-World@users.noreply.github.com>
2025-05-13 03:30:24 +0800

    Delete java directory

--- java/src/main/java/com/scooter/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.scooter.repository;
-
-import com.scooter.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
